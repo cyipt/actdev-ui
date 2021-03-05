a4358ef9f1369314dd51a50d526cf596029da1d5
mvl22 <martin@lucas-smith.co.uk>
2021-03-05 13:21:19 +0000

    Move segmented control code into JS out of HTML

--- js/actdev.js
+++ js/actdev.js
@@ -570,9 +570,12 @@ var actdev = (function ($) {
 					$('#' + checkboxId).click ();	// Also triggers event
 				}
 			});
+			
+			// Segmented control
+			actdev.segmentedControl ();
 		},
-
-
+		
+		
 		// Fetch all sites
 		fetchAllSites: function ()
 		{
@@ -1327,9 +1330,39 @@ var actdev = (function ($) {
 			});
 
 			_accessibilityChart.update();
-		}
+		},
 		
+		
+		// Segmented control
+		segmentedControl: function ()
+		{
+			// Constants
+			const SEGMENTED_CONTROL_BASE_SELECTOR = ".ios-segmented-control";
+			const SEGMENTED_CONTROL_INDIVIDUAL_SEGMENT_SELECTOR = ".ios-segmented-control .option input";
+			const SEGMENTED_CONTROL_BACKGROUND_PILL_SELECTOR = ".ios-segmented-control .selection";
+			
+			forEachElement(SEGMENTED_CONTROL_BASE_SELECTOR, (elem) => {
+				elem.addEventListener ('change', updatePillPosition);
+			});
+			window.addEventListener ('resize',
+				updatePillPosition
+			); // Prevent pill from detaching from element when window resized. Becuase this is rare I haven't bothered with throttling the event
+			
+			function updatePillPosition () {
+				forEachElement (SEGMENTED_CONTROL_INDIVIDUAL_SEGMENT_SELECTOR, (elem, index) => {
+					if (elem.checked) moveBackgroundPillToElement (elem, index);
+				});
+			}
+		
+			function moveBackgroundPillToElement (elem, index) {
+				document.querySelector (SEGMENTED_CONTROL_BACKGROUND_PILL_SELECTOR).style.transform = 'translateX(' + (elem.offsetWidth * index) + 'px)';
+			}
+			
+			// Helper functions
+			function forEachElement (className, fn) {
+				Array.from (document.querySelectorAll (className)).forEach (fn);
+			}
+		}
 	};
 	
 } (jQuery));
-
