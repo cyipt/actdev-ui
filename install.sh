e1f2194bed29f9dd99d5e98dda6aec1aea900da1
mvl22 <martin@lucas-smith.co.uk>
2025-01-29 23:15:04 +0000

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install the ActDev website
+
+
+# cd /var/www/
+# git clone https://github.com/cyipt/actdev-ui
+
+cd /var/www/actdev-ui/
+
+apt-get install -y apache2
+cp -pr .apache-vhost.conf.template /etc/apache2/sites-available/actdev.conf
+a2ensite actdev
+service apache2 restart
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarn.gpg >/dev/null
+echo "deb [signed-by=/usr/share/keyrings/yarn.gpg] https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list
+apt-get update
+apt-get install -y yarn
+
+yarn install
+
+cp -pr .config.js.template .config.js
+echo "Please enter your credentials in .config.js"
